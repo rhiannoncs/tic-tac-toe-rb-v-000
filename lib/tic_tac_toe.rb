f16aa8c25fe07b81bdc8a407c096cf0d8de9bdcf
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-20 03:09:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,4 +44,14 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+counter
+end
+
 
