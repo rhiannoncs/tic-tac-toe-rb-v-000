f63d219758493468a343687daff415e5d3f421c4
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-20 03:13:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    current_player = current_player(board)
+    move(board, index, current_player)
     display_board(board)
   else
     turn(board)
